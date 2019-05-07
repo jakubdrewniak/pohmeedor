eb9795d0eaa01bf97579e1fdd152843551b68458
jakubdrewniak <drewniak.jakub@gmail.com>
2019-05-07 18:15:13 +0200

    Size of the pie slices adjusted by script

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-// ( document ).ready(function() {
+function Pohmeedor() {
+    this.timeInMin = 25
+    this.timeInMillis = 25*60*1000
+    this.root = document.documentElement;
+    this.init()
+}
 
-// });
+Pohmeedor.prototype.init = function() {
+    this.timer = 0    
+    this.runTimer()
+    // this.startListeningTimerReset()
+}
+
+Pohmeedor.prototype.runTimer = function() {
+
+    this.root.style.setProperty('--first-half-rotate', "10deg");
+    this.root.style.setProperty('--second-half-rotate', "11deg");
+
+}
