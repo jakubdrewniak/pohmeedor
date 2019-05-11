eba78af4a339c52f0f43283f6ceab9b4bfe7080f
jakubdrewniak <drewniak.jakub@gmail.com>
2019-05-11 18:34:13 +0200

    Working reset button; resizing pie

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function Pohmeedor() {
-	this.timeInMin = 0.5
+	this.timeInMin = 0.1
 	this.timeInSec = this.timeInMin * 60
 	this.root = document.documentElement;
 	this.init()
@@ -7,43 +7,71 @@ function Pohmeedor() {
 
 Pohmeedor.prototype.init = function () {
 	this.timer = 0
+	this.pieResizer()
 	this.runTimer()
-	// this.startListeningTimerReset()
+}
+
+Pohmeedor.prototype.reset = function () {
+	this.root.style.setProperty('--first-half-rotate', "0deg");
+	this.root.style.setProperty('--second-half-rotate', "0deg");
+	this.runTimer()
+}
+
+Pohmeedor.prototype.pieResizer = function () {
+	let self = this
+
+	function setPieSize() {
+		let windowHeight = window.innerHeight
+		let windowWidth = window.innerWidth
+		self.root.style.setProperty('--pie-dimension', Math.min(windowHeight, windowWidth) / 2 + "px");
+	}
+
+	setPieSize()
+	window.addEventListener('resize', setPieSize);
 }
 
 Pohmeedor.prototype.runTimer = function () {
 	const self = this
-	this.root.style.setProperty('--first-half-rotate', "0deg");
-	this.root.style.setProperty('--second-half-rotate', "0deg");
 	let currentFHDegree = 0
 	let currentSHDegree = 0
 	const degreeForOneSecond = 360 / this.timeInSec
 	let secondHalfIntervalToken
+	let secondHalfStartPermitted = true
 
 
-	const timerForFirstHalf = setInterval(() => {
+	const starterForFirstHalf = setInterval(() => {
 		currentFHDegree += degreeForOneSecond
 		this.root.style.setProperty('--first-half-rotate', currentFHDegree + "deg")
 	}, 1000)
 
 	const stopperForFirstHalf = setTimeout(
-		() => { clearInterval(timerForFirstHalf) }, this.timeInSec / 2 * 1000
-	)
-
-	const starterForSecondHalf = setTimeout(
-		timerForSecondHalf, this.timeInSec / 2 * 1000
+		() => {
+			clearInterval(starterForFirstHalf)
+			timerForSecondHalf()
+		}, this.timeInSec / 2 * 1000
 	)
 
 	function timerForSecondHalf() {
-		secondHalfIntervalToken = setInterval(() => {
-			currentSHDegree += degreeForOneSecond
-			self.root.style.setProperty('--second-half-rotate', currentSHDegree + "deg")
-		}, 1000)
+		if (secondHalfStartPermitted)
+			secondHalfIntervalToken = setInterval(() => {
+				currentSHDegree += degreeForOneSecond
+				self.root.style.setProperty('--second-half-rotate', currentSHDegree + "deg")
+			}, 1000)
 	}
 
 	const stopperForSecondHalf = setTimeout(
-		() => {clearInterval(secondHalfIntervalToken)}, this.timeInSec * 1000
+		() => { clearInterval(secondHalfIntervalToken) }, this.timeInSec * 1000
 	)
+
+	function stopAll() {
+		secondHalfStartPermitted = false
+		clearInterval(starterForFirstHalf)
+		clearInterval(secondHalfIntervalToken)
+		self.reset()
+	}
+
+	let resetButton = document.querySelector('.reset-button')
+	resetButton.addEventListener("click", stopAll)
 }
 
 
